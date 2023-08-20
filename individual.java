6f533987e3880bfdc0bd2018ee53bb5826a7b8ab
AnandaThakur <138412024+AnandaThakur@users.noreply.github.com>
2023-08-20 21:11:22 +0545

    Create individual.java

---
+++ individual.java
@@ -0,0 +1,11 @@
+import java.io.Serializable;
+public class individual implements Serializable{
+    private String name;
+    private int age;
+    public individual(String name, int age){
+    this.name = name;
+    this.age =age;
+
+    }
+    //getters and setters go there
+}
